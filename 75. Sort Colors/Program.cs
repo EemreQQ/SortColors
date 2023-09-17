2f82648269f1df0d85a8c6c0004f0c720dfa4886
emre erol <emre_erol17@hotmail.com>
2023-09-17 23:07:36 +0300

    Proje dosyası ekle.

---
+++ 75. Sort Colors/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public void SortColors(params int[] nums)
+    {
+        // red,white,blue
+        //  0     1     2 şeklinde sıralansın
+        // 1<=n<=300
+        // Sort olmadan çözülmesini istiyor
+        if (nums.Length == 1) Console.Write(nums[0]);
+        else
+        {
+            int b = 0;
+            int value = 0;
+            int a = 0;
+            List<int> list = new List<int>();
+            while (value < 3)
+            {
+                a = Array.FindIndex(nums, b, x => x == value);
+                if (a == -1) { value++; a = 0; b = 0; continue; }
+                b = a + 1;
+                list.Add(nums[a]);
+            }
+            nums = list.ToArray();
+            foreach (var item in nums)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
